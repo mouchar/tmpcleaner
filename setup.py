616dcc93a93f882fd98aa12753475bdbf14f495c
Petr Benas <petr.benas@gooddata.com>
2015-05-25 09:18:32 +0200

    bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ class PyTest(TestCommand):
 # Parameters for build
 params = {
     'name': 'tmpcleaner',
-    'version': '1.0',
+    'version': '1.0.1',
     'packages': [
         'gdctmpcleaner',
         'gdctmpcleaner.logger'
